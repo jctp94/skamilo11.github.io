7fd9a1a21d98a5322bde910b4fd474f5212e44df
Camilo Torres <skamilo9406@gmail.com>
2015-10-29 10:22:50 -0500

    agregada segunda pregunta

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
 function corroborar() {
-	var respuestas= ["no"]
+	var respuestas= ["no","Hyper Text Markup Language"]
 	var check = document.getElementsByName("web");
 	var respuesta1=document.getElementsByName("respuesta");
 	if(respuesta1[0].id=="r1"){
@@ -14,6 +14,19 @@ function corroborar() {
 		}else{
 			alert("respuesta incorrecta");
 		}
+	}else if(respuesta1[0].id=="r2"){
+		for(var i=0; i<check.length; i++){
+			if(check[i].checked===true){
+				respuesta1=check[i].value;
+			}
+		}
+		alert(respuesta1);
+		if(respuesta1==respuestas[1]){
+			alert("Correcto sigue así!");
+			document.getElementById("cont").disabled =false;
+		}else{
+			alert("respuesta incorrecta");
+		}		
 	}
 	
 /*	
