5a48b7bd9408b875f044548b064dea48d7fcd4d0
Camilo Torres <skamilo9406@gmail.com>
2015-10-29 23:50:52 -0500

    hasta la pregunta 5

--- codigo.js
+++ codigo.js
@@ -1,35 +1,68 @@
-function corroborar() {
-	var respuestas= ["no","Hyper Text Markup Language"]
+function corroborar(a) {
+	var respuestas= ["no","Hyper Text Markup Language","<!--...-->","correcto"]
 	var check = document.getElementsByName("web");
 	var respuesta1=document.getElementsByName("respuesta");
-	if(respuesta1[0].id=="r1"){
+	if(respuesta1[a].id=="r1"){
 		for(var i=0; i<check.length; i++){
 			if(check[i].checked===true){
 				respuesta1=check[i].value;
 			}
 		}
-		if(respuesta1==respuestas[0]){
+		if(respuesta1=="correcto"){
 			alert("respuesta correcta, internet: redes hetereogeneas interconectadas; la web hace parte de internet y es un servicio de  que funciona con un protocolo HTTP");
-			document.getElementById("cont").disabled =false;
+			document.getElementById('p2').style.display = 'block';
+			document.getElementById('p1').style.display = 'none';
 		}else{
 			alert("respuesta incorrecta");
 		}
-	}else if(respuesta1[0].id=="r2"){
+	}else if(respuesta1[a].id=="r2"){
 		for(var i=0; i<check.length; i++){
 			if(check[i].checked===true){
 				respuesta1=check[i].value;
 			}
 		}
-		alert(respuesta1);
-		if(respuesta1==respuestas[1]){
+		if(respuesta1=="correcto"){
 			alert("Correcto sigue así!");
-			document.getElementById("cont").disabled =false;
+			document.getElementById('p3').style.display = 'block';
+			document.getElementById('p2').style.display = 'none';
 		}else{
 			alert("respuesta incorrecta");
-		}		
+		}
+	}else if(respuesta1[a].id=="r3"){
+		for(var i=0; i<check.length; i++){
+			if(check[i].checked===true){
+				respuesta1=check[i].value;
+			}
+		}
+		if(respuesta1=="correcto"){
+			alert("Eso!!");
+			document.getElementById('p4').style.display = 'block';
+			document.getElementById('p3').style.display = 'none';
+		}else{
+			alert("respuesta incorrecta");
+		}
+	}else if(respuesta1[a].id=="r4"){
+		for(var i=0; i<check.length; i++){
+			if(check[i].checked===true){
+				respuesta1=check[i].value;
+			}
+		}
+		if(respuesta1=="correcto"){
+			alert("Eso!!");
+			document.getElementById('p5').style.display = 'block';
+			document.getElementById('p4').style.display = 'none';			
+		}else{
+			alert("respuesta incorrecta");
+		}
+	}else if(respuesta1[a].id=="r5"){
+		var ct= document.getElementById('rta5').value;
+		if(ct=="post"){
+			alert("muy bien!!");
+			document.getElementById('p90').style.display = 'block';
+			document.getElementById('p5').style.display = 'none';			
+		}else{
+			alert("respuesta incorrecta");
+		}
 	}
-	
-/*	
-	
-*/
+
 }
